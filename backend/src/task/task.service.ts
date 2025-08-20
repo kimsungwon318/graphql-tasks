bcea387d5fbe27057dfa2d1f22e0fd775a39110b
kimsungwon318 <sungwon.kim@narimoto.co.jp>
2025-08-21 08:37:46 +0900

    userId add

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -2,24 +2,29 @@ import { Injectable } from '@nestjs/common';
 
 import { CreateTaskInput } from './dto/createTask.input';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Task } from 'generated/prisma';
+import { Task } from '@prisma/client';
 import { UpdateTaskInput } from './dto/updateTask.input';
 
 @Injectable()
 export class TaskService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getTasks(): Promise<Task[]> {
-    return await this.prismaService.task.findMany();
+  async getTasks(userId: number): Promise<Task[]> {
+    return await this.prismaService.task.findMany({
+      where: {
+        userId,
+      },
+    });
   }
 
   async createTask(createTaskInput: CreateTaskInput): Promise<Task> {
-    const { name, dueDate, description } = createTaskInput;
+    const { name, dueDate, description, userId } = createTaskInput;
     return await this.prismaService.task.create({
       data: {
         name,
         dueDate,
         description,
+        userId,
       },
     });
   }
