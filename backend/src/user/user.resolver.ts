c900e789261a446c32554e370d486498a1afc900
kimsungwon318 <sungwon.kim@narimoto.co.jp>
2025-08-23 09:27:57 +0900

    user add

---
+++ backend/src/user/user.resolver.ts
@@ -0,0 +1,24 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User as UserModel } from './models/user.model';
+import { UserService } from './user.service';
+import { CreateUserInput } from './dto/createUser.input';
+import { User } from '@prisma/client';
+
+@Resolver()
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => UserModel)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+  ): Promise<User> {
+    return await this.userService.createUser(createUserInput);
+  }
+
+  @Query(() => UserModel, { nullable: true })
+  async getUser(
+    @Args('email', { type: () => String }) email: string,
+  ): Promise<User> {
+    return await this.userService.getUser(email);
+  }
+}
